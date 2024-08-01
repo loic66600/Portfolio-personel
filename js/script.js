33a99a4d358204d3c40f6ce3087cdfb4b2cf6c54
loic <loic.rossignol66@gmail.com>
2024-08-01 09:27:54 +0200

    amelioration email

--- js/script.js
+++ js/script.js
@@ -8,18 +8,36 @@ function sendMail() {
 
     const serviceID = "service_fdxo5tr";
     const templateID = "template_5gji2h9";
+
     emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            // Réinitialiser les champs du formulaire
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
 
-        .then(
-            res => {
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("subject").value = "";
-                document.getElementById("message").value = "";
-                console.log(res);
-                alert("Message sent successfully");
-            }
-        )
-        .catch(err => console.log(err));
+            // Afficher le message de succès
+            const successMessage = document.getElementById("success-message");
+            successMessage.style.display = "block";
+            setTimeout(() => {
+                successMessage.style.display = "none";
+                // Rafraîchir la page après 5 secondes
+                window.location.reload();
+            }, 5000); // Afficher pendant 5 secondes
 
+            console.log(res);
+        })
+        .catch(err => {
+            // Afficher le message d'erreur
+            const errorMessage = document.getElementById("error-message");
+            errorMessage.style.display = "block";
+            setTimeout(() => {
+                errorMessage.style.display = "none";
+                // Rafraîchir la page après 5 secondes
+                window.location.reload();
+            }, 5000); // Afficher pendant 5 secondes
+
+            console.log(err);
+        });
 }
